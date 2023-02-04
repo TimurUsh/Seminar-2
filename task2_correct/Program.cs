4ad1aabc72d745a7c756a322ad2b15efcae38c10
Ushiyariv.Timur <uti2000@yandex.ru>
2023-02-04 13:51:40 +0300

    Add task2

---
+++ task2_correct/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] num = new int[2];
+string Result = "не является";
+for (int i = 0; i < 2; i++)
+{
+    num[i] = new Random().Next(1, 100);
+}
+if (num[0] / num[1] == num[1]) Result = Result.Replace("не ", "");
+
+System.Console.WriteLine($"Число {num[0]} {Result} квадратом числа {num[1]}");
+System.Console.WriteLine();
